95935b2957b1a56e8a7d85958271f329ec85f8c9
felix1415 <alexgray1415@gmail.com>
2021-02-28 18:26:24 +0000

    Update UT_Utils test with c_str, add -g flag to tests make file - add debug symbols for debugging

--- test/UT_Utils.cpp
+++ test/UT_Utils.cpp
@@ -2,7 +2,7 @@
 #include "../src/Utils.h"
 
 UTEST(Utils, colorToString) {
-    ASSERT_STREQ("red", Utils::colorToString(bitmap_image::color_plane::red_plane));
-    ASSERT_STREQ("green", Utils::colorToString(bitmap_image::color_plane::green_plane));
-    ASSERT_STREQ("blue", Utils::colorToString(bitmap_image::color_plane::blue_plane));
+    ASSERT_STREQ("red", Utils::colorToString(bitmap_image::color_plane::red_plane).c_str());
+    ASSERT_STREQ("green", Utils::colorToString(bitmap_image::color_plane::green_plane).c_str());
+    ASSERT_STREQ("blue", Utils::colorToString(bitmap_image::color_plane::blue_plane).c_str());
 }
