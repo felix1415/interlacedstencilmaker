9139d7e27b7b221ba0f53ef7405f8860c562b35d
felix1415 <alexgray1415@gmail.com>
2021-03-29 01:09:09 +0100

    Added grayscale test I forgot in the last commit

---
+++ test/UT_GrayTranslatedPixel.cpp
@@ -0,0 +1,147 @@
+#include "utest.h"
+#include "../src/Position.h"
+#include "../src/Pixel.h"
+#include "../src/GrayTranslatedPixel.h"
+#include <memory>
+
+UTEST(GreyTranslatedPixel, basicAll) {
+    std::unique_ptr<TranslatedPixel> tp;
+    Position pos(10,15);
+
+    rgb_t color;
+    color.red = 255;
+    color.green = 255;
+    color.blue = 255;
+
+    Pixel p(10, 15, color);
+
+    std::vector<Pixel> pixels;
+    pixels.push_back(p);
+
+    int steps = 5;
+    float tileSizeMM = 1.0f;
+
+    tp.reset(new GrayTranslatedPixel(pos, std::move(pixels), steps, tileSizeMM));
+
+    ASSERT_EQ(tp->getX(), 10);
+    ASSERT_EQ(tp->getY(), 15);
+
+    //Values are translated using the steps value
+    ASSERT_EQ(0, tp->getTranslatedColorValue(bitmap_image::color_plane::red_plane));
+    ASSERT_EQ(0, tp->getTranslatedColorValue(bitmap_image::color_plane::green_plane));
+    ASSERT_EQ(1.0f, tp->getTranslatedColorValue(bitmap_image::color_plane::blue_plane));
+}
+
+UTEST(GreyTranslatedPixel, red) {
+    std::unique_ptr<TranslatedPixel> tp;
+    Position pos(10,15);
+
+    rgb_t color;
+    color.red = 255;
+    color.green = 0;
+    color.blue = 0;
+
+    Pixel p(10, 15, color);
+
+    std::vector<Pixel> pixels;
+    pixels.push_back(p);
+
+    int steps = 5;
+    float tileSizeMM = 1.0f;
+
+    tp.reset(new GrayTranslatedPixel(pos, std::move(pixels), steps, tileSizeMM));
+
+    ASSERT_EQ(tp->getX(), 10);
+    ASSERT_EQ(tp->getY(), 15);
+
+    //Values are translated using the steps value
+    ASSERT_EQ(0, tp->getTranslatedColorValue(bitmap_image::color_plane::red_plane));
+    ASSERT_EQ(0, tp->getTranslatedColorValue(bitmap_image::color_plane::green_plane));
+    ASSERT_EQ(0.21f, tp->getTranslatedColorValue(bitmap_image::color_plane::blue_plane));
+}
+
+UTEST(GreyTranslatedPixel, green) {
+    std::unique_ptr<TranslatedPixel> tp;
+    Position pos(10,15);
+
+    rgb_t color;
+    color.red = 0;
+    color.green = 255;
+    color.blue = 0;
+
+    Pixel p(10, 15, color);
+
+    std::vector<Pixel> pixels;
+    pixels.push_back(p);
+
+    int steps = 5;
+    float tileSizeMM = 1.0f;
+
+    tp.reset(new GrayTranslatedPixel(pos, std::move(pixels), steps, tileSizeMM));
+
+    ASSERT_EQ(tp->getX(), 10);
+    ASSERT_EQ(tp->getY(), 15);
+
+    //Values are translated using the steps value
+    ASSERT_EQ(0, tp->getTranslatedColorValue(bitmap_image::color_plane::red_plane));
+    ASSERT_EQ(0, tp->getTranslatedColorValue(bitmap_image::color_plane::green_plane));
+    ASSERT_EQ(0.72f, tp->getTranslatedColorValue(bitmap_image::color_plane::blue_plane));
+}
+
+UTEST(GreyTranslatedPixel, blue) {
+    std::unique_ptr<TranslatedPixel> tp;
+    Position pos(10,15);
+
+    rgb_t color;
+    color.red = 0;
+    color.green = 0;
+    color.blue = 255;
+
+    Pixel p(10, 15, color);
+
+    std::vector<Pixel> pixels;
+    pixels.push_back(p);
+
+    int steps = 5;
+    float tileSizeMM = 1.0f;
+
+    tp.reset(new GrayTranslatedPixel(pos, std::move(pixels), steps, tileSizeMM));
+
+    ASSERT_EQ(tp->getX(), 10);
+    ASSERT_EQ(tp->getY(), 15);
+
+    std::cout << tp->getTranslatedColorValue(bitmap_image::color_plane::blue_plane) << std::endl;
+    //Values are translated using the steps value
+    ASSERT_EQ(0, tp->getTranslatedColorValue(bitmap_image::color_plane::red_plane));
+    ASSERT_EQ(0, tp->getTranslatedColorValue(bitmap_image::color_plane::green_plane));
+    ASSERT_EQ(0.07f, tp->getTranslatedColorValue(bitmap_image::color_plane::blue_plane));
+
+}
+
+UTEST(GreyTranslatedPixel, basic125) {
+    std::unique_ptr<TranslatedPixel> tp;
+    Position pos(10,15);
+
+    rgb_t color;
+    color.red = 125;
+    color.green = 125;
+    color.blue = 125;
+
+    Pixel p(10, 15, color);
+
+    std::vector<Pixel> pixels;
+    pixels.push_back(p);
+
+    int steps = 5;
+    float tileSizeMM = 1.0f;
+
+    tp.reset(new GrayTranslatedPixel(pos, std::move(pixels), steps, tileSizeMM));
+
+    ASSERT_EQ(tp->getX(), 10);
+    ASSERT_EQ(tp->getY(), 15);
+
+    //Values are translated using the steps value
+    ASSERT_EQ(0, tp->getTranslatedColorValue(bitmap_image::color_plane::red_plane));
+    ASSERT_EQ(0, tp->getTranslatedColorValue(bitmap_image::color_plane::green_plane));
+    ASSERT_EQ(0.4f, tp->getTranslatedColorValue(bitmap_image::color_plane::blue_plane));
+}
