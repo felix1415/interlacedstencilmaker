2a2c2762c7550e6ce57de5bcb569b3d988a5c51b
Felix <alexgray1415@gmail.com>
2022-05-26 20:12:09 +0100

    Added help option, rearranged some of the single letter options and added validation for bad options

--- src/App.cpp
+++ src/App.cpp
@@ -12,51 +12,70 @@ int App::run(int argc, char * argv[])
 {
     std::cout << "3 Colour Interlaced Stencil Creator" << std::endl;
 
-    cxxopts::Options options("Interlaced Stencil Maker", "A small utility for turning bitmap images into Interlaced rgb stencils");
+    cxxopts::Options options("Interlaced Stencil Maker", "A small utility for turning bitmap images into Interlaced stencils");
 
     try
     {
         options.add_options()
           ("d,debug", "Debugging mode", cxxopts::value<bool>()->default_value("false"))
           ("g,grayscale", "Generate grayscale stencil instead of an RGB collection of stencils", cxxopts::value<bool>()->default_value("false"))
-          ("c,wrgb", "Generate an RGB set of stencils to complement the grayscale", cxxopts::value<bool>()->default_value("false"))
-          ("w,width", "Width of image", cxxopts::value<uint16_t>())
-          ("h,height", "Height of image", cxxopts::value<uint16_t>())
+          ("w,wrgb", "Generate an RGB set of stencils to complement the grayscale", cxxopts::value<bool>()->default_value("false"))
+          ("W,width", "Width of image", cxxopts::value<uint16_t>())
+          ("H,height", "Height of image", cxxopts::value<uint16_t>())
           ("i,input", "Input file", cxxopts::value<std::string>())
           ("o,output", "Output folder", cxxopts::value<std::string>()->default_value("ISM"))
+          ("h,help", "This help message", cxxopts::value<bool>()->default_value("false"))
           ;
     }
     catch(const cxxopts::OptionSpecException & e)
     {
+        std::cout << options.help() << std::endl;
         std::cout << "ERROR: " << e.what() << std::endl;
     }
 
-    auto result = options.parse(argc, argv);
-
-    if(not result.count("input"))
+    try
     {
-        std::cerr << "Input file not defined" << std::endl;
-        return 0;
+        auto result = options.parse(argc, argv);
+
+        if (result.count("help"))
+        {
+            std::cout << options.help() << std::endl;
+            exit(0);
+        }
+        std::cerr << "After helpoo message" << std::endl;
+
+        if(not result.count("input"))
+        {
+            std::cerr << "Input file not defined" << std::endl;
+            return 0;
+        }
+
+        if(not result.count("width"))
+        {
+            std::cerr << "Width of picture not defined" << std::endl;
+            return 0;
+        }
+
+        if(not result.count("height"))
+        {
+            std::cerr << "Height of picture not defined" << std::endl;
+            return 0;
+        }
+
+        return Stencilator(result["width"].as<uint16_t>(), 
+                   result["height"].as<uint16_t>(), 
+                   result["input"].as<std::string>(),
+                   result["output"].as<std::string>(),
+                   result["debug"].as<bool>(),
+                   result["grayscale"].as<bool>(),
+                   result["wrgb"].as<bool>()
+                   ).execute();
+    }
+    catch(const cxxopts::OptionParseException & e)
+    {
+        std::cout << options.help() << std::endl;
+        std::cout << "ERROR: " << e.what() << std::endl;
     }
 
-    if(not result.count("width"))
-    {
-        std::cerr << "Width of picture not defined" << std::endl;
-        return 0;
-    }
-
-    if(not result.count("height"))
-    {
-        std::cerr << "Height of picture not defined" << std::endl;
-        return 0;
-    }
-
-    return Stencilator(result["width"].as<uint16_t>(), 
-                       result["height"].as<uint16_t>(), 
-                       result["input"].as<std::string>(),
-                       result["output"].as<std::string>(),
-                       result["debug"].as<bool>(),
-                       result["grayscale"].as<bool>(),
-                       result["wrgb"].as<bool>()
-                       ).execute();
+    return 1;
 }
